f3080f7d38fd4ca8d36c2a72a9d6b6b0dd68972a
Daniel Saboya <39964995+GunPocket@users.noreply.github.com>
2023-09-15 16:44:15 -0300

    🔨 Updating and refactoring lighthouse code

--- Assets/Scripts/LighthouseHandler.cs
+++ Assets/Scripts/LighthouseHandler.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class LighthouseHandler : MonoBehaviour {
     [SerializeField] private GameObject _sol;
     [SerializeField] private GameObject _maquete;
-    private float _rotation;
+    [SerializeField] private float _rotation;
     [Range(-360f, 360f)][SerializeField] private float _dia = 47;
     [Range(-360f, 360f)][SerializeField] private float _noite = 216;
 
-    // Update is called once per frame
+    [SerializeField] private OVRPassthroughLayer _passthroughLayer;
+
+    [SerializeField] private float _passtrhoughBrightness;
+    
     void Update() {
+        _passtrhoughBrightness = _sol.transform.rotation.z/2;
+        _passthroughLayer.SetColorMapControls(0, _passtrhoughBrightness);
+        print(_passtrhoughBrightness);
         _rotation = _sol.transform.eulerAngles.z;
         _maquete.SetActive(_rotation < _noite && _rotation > _dia);
     }
